823d6a8b80c7aaecc43aacd001683539edeaa4c7
Tushar Sharma <tushar-sharma@users.noreply.github.com>
2023-04-15 01:09:42 -0400

    adde ex3.rs

---
+++ ex3.rs
@@ -0,0 +1,19 @@
+fn main() {
+    // Macros in Rust are a powerful feature 
+    // that allows you to write code that generates other code at compile-time. 
+    // In the case of the println! macro, 
+    // it takes a string literal with placeholders for values to be printed 
+    // and generates code to print the formatted output to stdout at compile-time.
+
+    //example of a macro 
+    let distance = 2;
+    println("The distance is {}", distance);
+
+    //the println function is a regular function that takes a single string argument
+    // and prints it to stdout without any formatting
+    println("The distance is ", distance);
+
+    //println! macro is used for printing formatted output, 
+    //while the println function is used for printing a single string without any formatting.
+
+}
